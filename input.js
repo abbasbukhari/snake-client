c296ee67b474e453f5362b38901e0a3133e9838e
abbas <abbasbukhari2015@gmail.com>
2024-10-31 19:14:16 -0400

    Add canned messages for chat feature

--- input.js
+++ input.js
@@ -1,42 +1,34 @@
 // input.js
-
-// Stores the active TCP connection object.
 let connection;
 
-// Function to handle specific keypress actions
-const handleUserInput = function (key) {
-  if (key === '\u0003') { // '\u0003' is the "Ctrl + C" key command
-    console.log("Exiting game...");
-    process.exit();
-  }
-  
-  // Mapping WASD keys to movement commands
-  if (key === 'w') {
-    connection.write("Move: up");
-  }
-  if (key === 'a') {
-    connection.write("Move: left");
-  }
-  if (key === 's') {
-    connection.write("Move: down");
-  }
-  if (key === 'd') {
-    connection.write("Move: right");
-  }
-};
-
-// Function to set up stdin for handling keyboard input
 const setupInput = (conn) => {
   connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
-  // Event listener for stdin data (keyboard input)
   stdin.on("data", handleUserInput);
-
   return stdin;
 };
 
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+  // Movement commands
+  if (key === 'w') connection.write("Move: up");
+  if (key === 'a') connection.write("Move: left");
+  if (key === 's') connection.write("Move: down");
+  if (key === 'd') connection.write("Move: right");
+
+  // Canned messages
+  if (key === '1') connection.write("Say: Let's go!");
+  if (key === '2') connection.write("Say: Watch out!");
+  if (key === '3') connection.write("Say: Ssssssss!");
+  if (key === '4') connection.write("Say: Too close!");
+};
+
+// Export setupInput function
 module.exports = { setupInput };
