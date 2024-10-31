e94efc27ad1691656d3229cabab4201e29b75396
abbas <abbasbukhari2015@gmail.com>
2024-10-31 19:11:18 -0400

    Add keyboard control for snake movement

--- client.js
+++ client.js
@@ -11,37 +11,36 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+// Handle incoming data from the server
+  conn.on("data", (data) => {
+        console.log("Server says:", data);
+      });
+
   // Event handler for when the connection is established
   conn.on("connect", () => {
     console.log("Successfully connected to the game server!");
-    
-    // Send the name message to the server
     conn.write("Name: ABB"); // 'ABB' are the chosen initials
 
 
-    // Send a single "Move: up" command
-    conn.write("Move: up");
+    // // Send a single "Move: up" command
+    // conn.write("Move: up");
 
-    // Send multiple successive "Move: up" commands with slight delay
-    setTimeout(() => conn.write("Move: up"), 0);
-    setTimeout(() => conn.write("Move: up"), 50);
-    setTimeout(() => conn.write("Move: up"), 100);
+    // // Send multiple successive "Move: up" commands with slight delay
+    // setTimeout(() => conn.write("Move: up"), 0);
+    // setTimeout(() => conn.write("Move: up"), 50);
+    // setTimeout(() => conn.write("Move: up"), 100);
 
-    // Continuous movement using setInterval
-    const moveInterval = setInterval(() => {
-      conn.write("Move: up");
-    }, 50);
+    // // Continuous movement using setInterval
+    // const moveInterval = setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
 
-    // Stop the continuous movement after 500ms
-    setTimeout(() => clearInterval(moveInterval), 500);
+    // // Stop the continuous movement after 500ms
+    // setTimeout(() => clearInterval(moveInterval), 500);
   });
 
-  // Handle incoming data from the server
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
 
   return conn;
 };
 
-module.exports = connect;
+module.exports = { connect };
--- input.js
+++ input.js
@@ -1,25 +1,42 @@
 // input.js
 
+// Stores the active TCP connection object.
+let connection;
+
 // Function to handle specific keypress actions
 const handleUserInput = function (key) {
-    if (key === '\u0003') { // '\u0003' is the "Ctrl + C" key command
-      console.log("Exiting game...");
-      process.exit();
-    }
-  };
+  if (key === '\u0003') { // '\u0003' is the "Ctrl + C" key command
+    console.log("Exiting game...");
+    process.exit();
+  }
   
-  // Function to set up stdin for handling keyboard input
-  const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-  
-    // Event listener for stdin data (keyboard input)
-    stdin.on("data", handleUserInput);
-  
-    return stdin;
-  };
-  
-  module.exports = { setupInput };
-  
+  // Mapping WASD keys to movement commands
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+};
+
+// Function to set up stdin for handling keyboard input
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // Event listener for stdin data (keyboard input)
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = { setupInput };
